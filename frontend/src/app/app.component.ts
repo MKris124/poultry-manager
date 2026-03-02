0f2eac22a9b38de06b903da4b8cc79b1a64fbffc
MKris124 <kristofmora124@gmail.com>
2026-03-02 18:58:31 +0100

    version update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   title = 'Baromfi Menedzser';
   isDarkMode: boolean = false;
 
-  appVersion: string = 'v1.0.6';
+  appVersion: string = 'v1.0.7';
 
   ngOnInit() {
     if (!sessionStorage.getItem('first_reload_done')) {
